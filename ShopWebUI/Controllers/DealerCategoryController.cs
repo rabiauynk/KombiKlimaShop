6ee1612ae730acb09634bcee3db83227d4f4bffc
rabiauyanik1@365saglam.com <rabiauynk05@gmail.com>
2024-08-22 14:52:39 +0300

    "commit"

--- ShopWebUI/Controllers/DealerCategoryController.cs
+++ ShopWebUI/Controllers/DealerCategoryController.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ShopWebUI.Dtos.DealerCategoryDtos;
 
 namespace ShopWebUI.Controllers
 {
 	public class DealerCategoryController : Controller
 	{
-		private readonly	IHttpClientFactory httpClientFactory;
+		private readonly IHttpClientFactory _httpClientFactory;
 
 		public DealerCategoryController(IHttpClientFactory httpClientFactory)
 		{
-			this.httpClientFactory = httpClientFactory;
+			_httpClientFactory = httpClientFactory;
 		}
 
-		public async Task <IActionResult> Index()
+		public async Task<IActionResult> Index()
 		{
 			var client = _httpClientFactory.CreateClient();
-			var responseMessage=await client 
+			var responseMessage = await client.GetAsync("https://localhost:7052/api/DealerCategory");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values=JsonConvert.DeserializeObject<List<ResultDealerCategoryDto>>(jsonData);
+				return View(values);
+			}
 			return View();
 		}
 	}
---
+++ ShopWebUI/Dtos/DealerCategoryDtos/ResultDealerCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ShopWebUI.Dtos.DealerCategoryDtos
+{
+	public class ResultDealerCategoryDto
+	{
+		public int DealerCategoryID { get; set; }
+		public string CategoryName { get; set; }
+	}
+}
